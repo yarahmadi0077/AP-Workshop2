6daffc823e450e34600fa018797cc793a0fc5ed9
Mohammad Yarahmadi <yarahmadi0077@gmail.com>
2024-03-04 14:56:48 +0330

    second commit that added Lab.java and testcase.txt and applied some changes on Main.java and Student.java

--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,36 @@
+import java.util.Scanner;
+
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        Student std1 = new Student("Mohammad", "Yarahmadi", "12345678", "19.2");
-//        std1.setStdId("12345678");
+        Scanner input = new Scanner(System.in);
+        System.out.println("please enter the number of students:......");
+        int n = input.nextInt();
+        Student[] labClass = new Student[n];
+        Lab test1 = new Lab("Pouria Azizi", "Mondey", n);
+        System.out.println("enter the students' information like following example :");
+        System.out.println("ex --->>>> Alireza Rezaie <idCode> <grade>");
+        for (int i = 0; i < n; i++) {
+            String s1 = input.next(), s2 = input.next(), s3 = input.next(), s4 = input.next();
+            labClass[i] = new Student(s1, s2, s3, s4);
+        }
+        test1.setStudents(labClass);
+        test1.setavgGrade(labClass);
+//        Student std1 = new Student("Mohammad", "Yarahmadi", "1234567", "19.2");
+//        Student std2 = new Student("Arsalan", "mousivand", "1234567", "2.6");
+        for (int i = 0; i < n; i++) {
+            labClass[i].printInfo();
+        }
+        /*System.out.println("............");
         std1.printInfo();
-//        System.out.println(std1.getStdId());
+        std2.printInfo();*/
+        test1.printLabInfo();
     }
 }
+
+/*
+AP-Workshop02
+Mohammad Yarahmadi
+40231059
+ */
